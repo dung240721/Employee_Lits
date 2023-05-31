9a940231433262a33d424ca1e96e4ab188a3aeac
dung240721 <93905176+dung240721@users.noreply.github.com>
2023-05-31 16:25:36 +0700

    Initial commit

---
+++ src/main/java/MVC/StudentServlet.java
@@ -0,0 +1,120 @@
+package MVC;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/students")
+public class StudentServlet extends HttpServlet {
+    private List<Student> studentList;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        studentList = new ArrayList<>();
+        studentList.add(new Student(1, "Han Nguyen Minh Anh", "T2108A1", 9.9));
+        studentList.add(new Student(2, "Ho Anh Dung", "T2108A1", 5.9));
+        studentList.add(new Student(3, "Minh Anh", "T2108A1", 8.0));
+        studentList.add(new Student(4, "Nguyen Manh Dung", "T2108A1", 10));
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action == null) {
+            action = "list";
+        }
+        switch (action) {
+            case "new":
+                showNewForm(request, response);
+                break;
+            case "create":
+                createStudent(request, response);
+                break;
+            case "edit":
+                showEditForm(request, response);
+                break;
+            case "update":
+                updateStudent(request, response);
+                break;
+            case "delete":
+                deleteStudent(request, response);
+                break;
+            default:
+                listStudents(request, response);
+                break;
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    private void listStudents(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("studentList", studentList);;
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Student-list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Student-form.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void createStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String grade = request.getParameter("grade");
+        double scores = Double.parseDouble(request.getParameter("scores"));
+        int id = studentList.size() + 1;
+
+        Student newStudent = new Student(id, name, grade, scores);
+        studentList.add(newStudent);
+
+        response.sendRedirect("students");
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Student student = getStudentById(id);
+
+        request.setAttribute("student", student);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Student-form.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void updateStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String grade = request.getParameter("grade");
+        double scores = Double.parseDouble(request.getParameter("scores"));
+
+        Student student = getStudentById(id);
+        student.setName(name);
+        student.setGrade(grade);
+        student.setScores(scores);
+
+        response.sendRedirect("students");
+    }
+    private void deleteStudent(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Student student = getStudentById(id);
+        studentList.remove(student);
+
+        response.sendRedirect("students");
+    }
+
+    private Student getStudentById(int id) {
+        for (Student student : studentList) {
+            if (student.getId() == id) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+}
